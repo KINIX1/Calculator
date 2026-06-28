982fa0182f0b9a86499c3f0d20bbcb4e80579732
KINIX1 <fgholi479@gmail.com>
2026-06-28 12:35:03 +0300

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,18 @@
+import tkinter as Tk
+from tkinter import Tk, Canvas
+window = Tk.Tk()
+window.title("Calculator")
+window.geometry("1530x785")
+window.configure(bg = "white")
+canvas = Canvas(window, width = 785, height = 1530)
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
